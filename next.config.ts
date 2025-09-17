bbb56c1e3c2e4d071db1dc4dee6341754f7905e7
Arffei <arffei.storm@gmail.com>
2025-09-17 23:14:08 +0700

    666

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbopack: true,
+  },
+}
+
+module.exports = nextConfig
